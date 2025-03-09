ded49f9dd7f28d999105c499b561a642fcd9ae00
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2025-03-10 01:56:13 +0530

    Update useWebSocket.ts

--- apps/frontend/hooks/useWebSocket.ts
+++ apps/frontend/hooks/useWebSocket.ts
@@ -36,34 +36,4 @@ export const useWebSocket = () => {
 
 
 
-
-// import { useState, useEffect } from "react";
-// import { io } from "socket.io-client";
-
-// const SERVER_URL = "ws://127.0.0.1:5000"; // Ya phir "ws://192.168.1.34:5000"
-
-// export const useWebSocket = () => {
-//   const [frame, setFrame] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const socket = io(SERVER_URL);
-
-//     socket.on("connect", () => {
-//       console.log("Connected to WebSocket Server ✅");
-//     });
-
-//     socket.on("video_frame", (data) => {
-//       setFrame(`data:image/jpeg;base64,${data.frame}`);
-//     });
-
-//     socket.on("disconnect", () => {
-//       console.log("Disconnected ❌");
-//     });
-
-//     return () => {
-//       socket.disconnect();
-//     };
-//   }, []);
-
-//   return frame;
-// };
+ 
